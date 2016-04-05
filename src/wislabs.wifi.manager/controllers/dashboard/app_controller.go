86641233c5761bfa75b1667b40646fd2f0d664c5
Sandun Priyanka <desilva.nsp@gmail.com>
2016-04-06 00:33:59 +0530

    Apply new column value groupid with each groupname in app creation appgroups table.

--- src/wislabs.wifi.manager/controllers/dashboard/app_controller.go
+++ src/wislabs.wifi.manager/controllers/dashboard/app_controller.go
@@ -166,7 +166,7 @@ func AddDashboardAppGroups(appGroup *[]dao.DashboardAppGroup, appId int64) error
 		return err
 	}
 	for i := 0; i < len(*appGroup); i++ {
-		_, err = stmtIns.Exec(appId, (*appGroup)[i].GroupName)
+		_, err = stmtIns.Exec(appId,i+1, (*appGroup)[i].GroupName)
 	}
 	return err
 }
